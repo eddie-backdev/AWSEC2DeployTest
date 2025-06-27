faac16ee6a25732fbd9afcbcd4bfb81cb6ea041f
Eddie Lee <devleehy@gmail.com>
2025-06-27 11:20:43 +0900

    deploy 추가

--- src/test/java/my/awsec2edeploytest/Awsec2EDeployTestApplicationTests.java
+++ src/test/java/my/awsec2edeploytest/Awsec2EDeployTestApplicationTests.java
@@ -1,13 +1,18 @@
 package my.awsec2edeploytest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Awsec2EDeployTestApplicationTests {
 
+    String str1 = "hello";
+
     @Test
-    void contextLoads() {
+    void test() {
+        assertEquals("hello", str1);
     }
 
 }
